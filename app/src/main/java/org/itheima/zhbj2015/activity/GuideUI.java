743c68f0cfccce7737d4d6f6792a3cc039d905ce
yuqiqi <yuqiqi17786@163.com>
2015-08-13 17:30:36 +0800

    第二次更新测试

--- app/src/main/java/org/itheima/zhbj2015/activity/GuideUI.java
+++ app/src/main/java/org/itheima/zhbj2015/activity/GuideUI.java
@@ -7,30 +7,79 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 /**
  * Created by yuqiqi on 2015/8/13.
  */
-public class GuideUI extends BaseActivity {
+public class GuideUI extends BaseActivity implements ViewPager.OnPageChangeListener{
 
     private ViewPager mVp;
+    private Button mButton;
+
+    private int[] iconRes = new int[]{
+            R.mipmap.guide_1,
+            R.mipmap.guide_2,
+            R.mipmap.guide_3,
+    };
+
+    private ArrayList<ImageView> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.guide_ui);
         initView();
+        initData();
+    }
+
+    private void initData() {
+        mDatas = new ArrayList<ImageView>();
+        for (int i = 0; i < iconRes.length; i++) {
+            ImageView imageView = new ImageView(GuideUI.this);
+            imageView.setImageResource(iconRes[i]);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            mDatas.add(imageView);
+        }
+        mVp.setAdapter(new GuideAdapter());
+        mVp.setOnPageChangeListener(this);
     }
 
     private void initView() {
         mVp = (ViewPager) findViewById(R.id.guideViewpager);
-        mVp.setAdapter(new GuideAdapter());
+        mButton = (Button) findViewById(R.id.guideBt);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if (position==mDatas.size()-1){
+            mButton.setVisibility(View.VISIBLE);
+        }else{
+            mButton.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 
     private class GuideAdapter extends PagerAdapter {
 
         @Override
         public int getCount() {
+
+            if (mDatas != null) {
+                return mDatas.size();
+            }
             return 0;
         }
 
@@ -41,7 +90,9 @@ public class GuideUI extends BaseActivity {
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            return super.instantiateItem(container, position);
+            ImageView imageView = mDatas.get(position);
+            container.addView(imageView);
+            return imageView;
         }
 
         @Override
